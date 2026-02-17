7522a295ea241772a9f26e179b378ba45d23a329
Jonathon Wang <jwang2672@wisc.edu>
2026-02-17 16:06:26 -0600

    added comments to header

--- minilab3/spart.v
+++ minilab3/spart.v
@@ -17,18 +17,52 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module spart(
+module spart (
     input clk,
     input rst,
-    input iocs,
-    input iorw,
-    output rda,
-    output tbr,
-    input [1:0] ioaddr,
-    inout [7:0] databus,
-    output txd,
-    input rxd
-    );
+
+
+    //------------communicate to CPU-------------------------------
+    input iocs,  //I/O Chip Select
+
+    input iorw,  //Determines the direction of data transfer between the
+                 //Processor and SPART. For a Read (IOR/W=1), data is transferred
+                 //from the SPART to the Processor and for a Write (IOR/W=0),
+                 //data is transferred from the processor to the SPART.
+
+    output rda,  //Receive Data Available - Indicates that a byte of data has been received and is
+                 //ready to be read from the SPART to the Processor
+
+    output tbr, //Transmit Buffer Ready - Indicates that the transmit buffer in the SPART is ready
+                //to accept a byte for transmission.
+
+    input [1:0] ioaddr,  //A 2-bit address bus used to select the particular register that
+                         //interacts with the DATABUS during an I/O operation
+                        // -------------------------------------------------------------
+                        // SPART Register Address Mapping
+                        // -------------------------------------------------------------
+                        // IOADDR  | SPART Register
+                        // --------|-----------------------------------------------------
+                        //   2'b00 | Transmit Buffer (IOR/W = 0)
+                        //         | Receive Buffer  (IOR/W = 1)
+                        //
+                        //   2'b01 | Status Register  (IOR/W = 1)
+                        //
+                        //   2'b10 | DB (Low)  Division Buffer
+                        //
+                        //   2'b11 | DB (High) Division Buffer
+                        // -------------------------------------------------------------
+
+
+    inout [7:0] databus,  // An 8-bit, 3-state bidirectional bus used to transfer data and
+                          // control information between the Processor and the SPART.
+    //---------------------------------------------------------
+
+    //-------------communicate to other spark------------------
+    output txd,  //transmit
+    input  rxd   //receive
+    //---------------------------------------------------------
+);
 
 
 endmodule
